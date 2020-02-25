ecce012d6c19980c63bb19b68e06c04365b3f6fd
evil-process-watchdog-killer <watchdoguser@uniposta.de>
2020-02-25 09:54:01 +0100

    IWTMIMI

--- top1.sh
+++ top1.sh
@@ -4,7 +4,7 @@ echo "$loadlastmin"
 if [ "101" -ge "$loadlastmin"  ]
 then
 	#echo "nothing to do"
-        logger "All fine."
+        logger "CPU WD - ...All fine."
 	exit 0
 fi
 
@@ -12,12 +12,13 @@ ps ahux --sort=-c | awk 'NR<=1{printf"XX%2dXX\n",$2}' > ~/evil-process-watchdog-
 ps ahux --sort=-c | awk 'NR<=1{printf"%2d\n",$2}' > ~/evil-process-watchdog-killer/firstPID.log
 cat ~/evil-process-watchdog-killer/first.log >> ~/evil-process-watchdog-killer/top1.log
 evil=`cat ~/evil-process-watchdog-killer/first.log`
-echo "$evil"
+logger  "CPUWATCHDOG detected somebody to kill:: $evil"
 cat ~/evil-process-watchdog-killer/first.log
 evillivetime=`cat ~/evil-process-watchdog-killer/top1.log |grep  "$evil"| wc -l`
-echo "---$evillivetime--- Check for last 5 mins..."
+logger  "---$evillivetime--- Check for last 5 mins..."
 if [ "$evillivetime" -ge "5" ]; then
 	evilpid=`cat ~/evil-process-watchdog-killer/firstPID.log`
-	echo "IWANTTOKILLITKILLIT!!!---$evillivetime---: $evilpid ========="
+	logger "IWANTTOKILLITKILLIT!!!---$evillivetime---: $evilpid ========="
+	echo "IWANTTOKILLITKILLIT!!!---$evillivetime---: $evilpid =========" >>  ~/evil-process-watchdog-killer/Watchdog6.log
 	echo "$evilpid">~/evil-process-watchdog-killer/PIDTOKILL
 fi
